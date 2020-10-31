3faa7f45a20189d2add8447b0a7683be7da7985e
Nil patel <nilpatel0606@gmail.com>
2020-10-31 18:12:05 +0530

    enhance security

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x1!)&sexje1_gh_&!)o(z*vbd5v(5_zhzbl&(sh%q*3l)0d!h('
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default=True)
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +76,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
 'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'DBTB',
-        'USER':'postgres',
-        'PASSWORD':'Nil@1998',
-        'HOST':'localhost'
+        'ENGINE': config('ENGINE'),
+        'NAME':config('NAME'),
+        'USER':config('USER'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('HOST'),
     }
 }
 
