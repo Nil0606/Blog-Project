cb865c020e0f5e97a20d98b5406a45ed55a1491c
Nil patel <nilpatel0606@gmail.com>
2020-11-11 14:00:10 +0530

    Add Unlike Feature

--- theblog/views.py
+++ theblog/views.py
@@ -23,13 +23,23 @@ class ArticleDetailView(DetailView):
         context=super(ArticleDetailView, self).get_context_data()
         context['cat_menu']=cat_menu
         stuff=get_object_or_404(Post,id=self.kwargs['pk'])
+        liked=False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked=True
         total_likes=stuff.total_likes()
         context['total_likes']=total_likes
+        context['liked']=liked
         return context
 
 def Likeview(request,pk):
     post=get_object_or_404(Post,id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked=False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked=False
+    else:
+        post.likes.add(request.user)
+        liked=True
     return HttpResponseRedirect(reverse_lazy('article-detail',args=[str(pk)]))
 
 class AddpostView(CreateView):
