58f136a6221968b1a3e0e3339aac6e118bbb45a4
Nil patel <nilpatel0606@gmail.com>
2020-11-12 17:39:39 +0530

    improve registration page

--- members/views.py
+++ members/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from .forms import Registrationform
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class =Registrationform
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
